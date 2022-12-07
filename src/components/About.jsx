245909436200ac0a4bbfe10fa4eb7f1042bc22e4
Baye <bayisatamene17@gmail.com>
2022-12-07 08:30:01 -0800

    push to github

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Website development is very important in todays world.
+          We are going to use different computer language to develop useful software now on.
+          Website development is very important in todays world.
+          We are going to use different computer language to develop useful software now on.
+          Website development is very important in todays world.
+          We are going to use different computer language to develop useful software now on.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+        Website development is very important in todays world.
+          We are going to use different computer language to develop useful software now on.
+          Website development is very important in todays world.
+          We are going to use different computer language to develop useful software now on.
+          Website development is very important in todays world.
+          We are going to use different computer language to develop useful software now on.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
